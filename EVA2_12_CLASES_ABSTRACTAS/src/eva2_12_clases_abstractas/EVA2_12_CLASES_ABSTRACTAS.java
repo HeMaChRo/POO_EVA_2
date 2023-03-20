228d0c8bff64dae18238c31155b27d63c19667fd
Hemachro <124392305+HeMaChRo@users.noreply.github.com>
2023-03-20 15:02:58 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PruebaAbstracta pruebaAbs = new PruebaAbstracta();
+    }
+    
+}
+
+class prueba{
+}
+
+abstract class PruebaAbstracta{
+}
